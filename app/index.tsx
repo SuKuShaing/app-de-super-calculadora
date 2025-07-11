7b067e6419960d373acf5b0afe05a0bebbee9c5b
Sebastián Sanhueza Tapia <sebastian.sanhueza.t@usach.cl>
2025-07-11 17:43:26 -0400

    se añaden las demás filas de la calculadora

--- app/index.tsx
+++ app/index.tsx
@@ -16,7 +16,7 @@ const CalculatorApp = () => {
 				<ThemeText variant="p">Hola Mundo</ThemeText>
 			</View>
 
-			{/* Fila de botones */}
+			{/* 1ra Fila de botones */}
 			<View style={globalStyles.row}>
 				<CalcularButton
 					label="C"
@@ -44,9 +44,91 @@ const CalculatorApp = () => {
 				/>
 				<CalcularButton
 					label="÷"
+					color={Colors.orange}
 					onPress={() => {
 						"÷";
 					}}
+				/>
+			</View>
+
+			{/* 2da Fila de botones */}
+			<View style={globalStyles.row}>
+				<CalcularButton
+					label="7"
+					onPress={() => console.log("7")}
+				/>
+				<CalcularButton
+					label="8"
+					onPress={() => console.log("8")}
+				/>
+				<CalcularButton
+					label="9"
+					onPress={() => console.log("9")}
+				/>
+				<CalcularButton
+					label="x"
+					color={Colors.orange}
+					onPress={() => console.log("x")}
+				/>
+			</View>
+
+			{/* 3ra Fila de botones */}
+			<View style={globalStyles.row}>
+				<CalcularButton
+					label="4"
+					onPress={() => console.log("4")}
+				/>
+				<CalcularButton
+					label="5"
+					onPress={() => console.log("5")}
+				/>
+				<CalcularButton
+					label="6"
+					onPress={() => console.log("6")}
+				/>
+				<CalcularButton
+					label="-"
+					color={Colors.orange}
+					onPress={() => console.log("-")}
+				/>
+			</View>
+
+			{/* 4ta Fila de botones */}
+			<View style={globalStyles.row}>
+				<CalcularButton
+					label="1"
+					onPress={() => console.log("1")}
+				/>
+				<CalcularButton
+					label="2"
+					onPress={() => console.log("2")}
+				/>
+				<CalcularButton
+					label="3"
+					onPress={() => console.log("3")}
+				/>
+				<CalcularButton
+					label="+"
+					color={Colors.orange}
+					onPress={() => console.log("+")}
+				/>
+			</View>
+
+			{/* 5ta Fila de botones */}
+			<View style={globalStyles.row}>
+				<CalcularButton
+					label="0"
+					doubleSize
+					onPress={() => console.log("0")}
+				/>
+				<CalcularButton
+					label="."
+					onPress={() => console.log(".")}
+				/>
+				<CalcularButton
+					label="="
+					color={Colors.orange}
+					onPress={() => console.log("=")}
 				/>
 			</View>
 		</View>
--- components/CalcularButton.tsx
+++ components/CalcularButton.tsx
@@ -1,11 +1,15 @@
 import { Colors } from "@/constants/Colors";
 import { globalStyles } from "@/styles/global-styles";
+import * as Haptics from "expo-haptics";
 import { Pressable, Text } from "react-native";
+
+
 
 interface Props {
 	label: string;
 	color?: string;
 	blackText?: boolean;
+	doubleSize?: boolean;
 	onPress?: () => void;
 }
 
@@ -13,12 +17,28 @@ const CalcularButton = ({
 	label,
 	color = Colors.darkGray,
 	blackText = false,
+	doubleSize = false,
 	onPress,
 }: Props) => {
 	return (
-		<Pressable style={{...globalStyles.button, backgroundColor: color}} onPress={onPress}>
-			<Text style={{...globalStyles.buttonText, color: blackText ? Colors.background : Colors.textPrimary}}>{label}</Text>
-            {/* {...globalStyles.buttonText, se coloca primero, entonces al llegar los props de color sobre escriben los valores que globalStyles trajo} */}
+		<Pressable
+			style={({ pressed }) => ({
+				...globalStyles.button,
+				backgroundColor: color,
+				opacity: pressed ? 0.8 : 1,
+				width: doubleSize ? 180 : 80,
+			})}
+			// {...globalStyles.buttonText, se coloca primero, entonces al llegar los props de color sobre escriben los valores que globalStyles trajo}
+			onPress={() => Haptics.selectionAsync()}
+		>
+			<Text
+				style={{
+					...globalStyles.buttonText,
+					color: blackText ? Colors.background : Colors.textPrimary,
+				}}
+			>
+				{label}
+			</Text>
 		</Pressable>
 	);
 };
